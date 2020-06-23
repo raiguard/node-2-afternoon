55a32ee38a4ab02b05fe9b3a6aa28c74f9c3d603
raiguard <caleb.heuer@gmail.com>
2020-06-23 14:04:48 -0600

    NPM init, steps 1-3

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const port = 3001;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
